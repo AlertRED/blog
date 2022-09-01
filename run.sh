5999dcd38629a7c94e1c568fc5fd61c79e9c8b05
Aleksandr <drammtv@gmail.com>
2022-09-01 17:51:52 +0300

    Changed deploy algoritm run.sh and deploy.yml

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-
-yum update
-yum install -y python3 python3-pip npm
+yum update -y
+yum install -y python3 python3-pip npm lsof
 iptables -I INPUT -p tcp --dport 8000 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -I INPUT -p tcp --dport 4173 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080
+kill -9 $(lsof -t -i:8000)
+kill -9 $(lsof -t -i:8080)
+
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.prod.txt
 
 cd backend
 python3 manage.py migrate
-nohup $(gunicorn blog.asgi:application -w 2 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000) &
+gunicorn blog.asgi:application -w 2 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --daemon
 
 cd ../frontend
 npm install
 npm run build
-nohup $(npm run preview -- --host 0.0.0.0) &
-
-exit
+nohup npm run preview -- --host 0.0.0.0 > nohup.out 2> nohup.err < /dev/null &
