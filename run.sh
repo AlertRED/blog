cd52db72cc18de119d9fbe8e27a466ea88dcb19d
Aleksandr <drammtv@gmail.com>
2022-09-05 13:53:08 +0300

    Added 80 port

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 yum update -y
 yum install -y python3 python3-pip npm lsof
 iptables -I INPUT -p tcp --dport 8000 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080
+iptables -I INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+# iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080
 kill -9 $(lsof -t -i:8000)
-kill -9 $(lsof -t -i:8080)
+kill -9 $(lsof -t -i:80)
 
 python3 -m venv venv
 source venv/bin/activate
