8cff5e2fb98980304fd1f6a5bc3d42b28437c9e8
Aleksandr <drammtv@gmail.com>
2022-10-07 18:46:29 +0300

    Bugfix with scrolltop when page changed

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,9 @@ import router from "./router";
 import "./assets/main.css";
 import "./assets/icomoon.css";
 
+router.beforeEach(function () {
+    window.scrollTo(0, 0);
+})
 
 const app = createApp(App);
 app.use(router);
