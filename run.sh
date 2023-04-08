33c995add1685df0372257367e9dea91185db4c2
Aleksandr <drammtv@gmail.com>
2023-04-09 02:16:12 +0300

    Bugfix frontend assets

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-yum update -y
 yum install -y python3 python3-pip npm lsof nginx
 iptables -I INPUT -p tcp --dport 8000 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -I INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
@@ -11,7 +10,6 @@ kill -9 $(lsof -t -i:5173)
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.prod.txt
-
 cd backend
 python3 manage.py migrate
 gunicorn blog.asgi:application -w 2 -k uvicorn.workers.UvicornWorker --bind 127.0.0.1:8001 --daemon
